4855c62ea6380db53506541cdb9f95aed06ebb59
PriyankaDathriB <74810406+PriyankaDathriB@users.noreply.github.com>
2021-05-11 17:53:02 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -17,21 +17,10 @@ class Game {
 
   async start(){
     if(gameState === 0){
-      player = new Player();
-      var playerCountRef = await database.ref('playerCount').once("value");
-      if(playerCountRef.exists()){
-        playerCount = playerCountRef.val();
-        player.getCount();
-      }
       form = new Form()
       form.display();
     }
   }
 
-  play(){
-    form.hide();
-    textSize(30);
-    text("Game Start", 120, 100)
-   
-  }
+  
 }
